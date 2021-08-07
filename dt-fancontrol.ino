32d5b5bd0a20aa7dc9c8053e1566c9b9c642abc0
Maurizio Hostettler <maurizio.hostettler@gmail.com>
2021-08-07 18:37:12 +0200

    disable display if dT < 4.0

--- dt-fancontrol.ino
+++ dt-fancontrol.ino
@@ -10,35 +10,40 @@ void setPwmDuty(byte duty);
 void parseSerial();
 
 // Constants
-const byte OC1A_PIN = 9; // PWM Pin for fan to controll
-const word PWM_FREQ_HZ = 25000; //Adjust this value to adjust the frequency
+const byte OC1A_PIN = 9;  // PWM Pin for fan to controll
+const word PWM_FREQ_HZ = 25000;  // Adjust this value to adjust the frequency
 const word TCNT1_TOP = 16000000 / (2 * PWM_FREQ_HZ);
-const int R = 10000; // R = 10KΩ -> voltage divider resistance
+const int R = 10000;  // R = 10KΩ -> voltage divider resistance
 
 // Variables
 unsigned long lastRefreshTime;
-float RT, VR, ln, TX, T0, VRT, sExp, sRad, sAmb, deltaT, VCC;
-float s_slope, s_attack; // sigmoid variables
+float RT, VR, ln, TX, T0, VRT, sExp, sRad, sAmb, deltaT, deltaTOld, VCC;
+float s_slope, s_attack;  // sigmoid variables
 byte  pwm, static_pwm;
 int   s_min, s_max, readCount;
+bool  oled_active;  // oled display status
+
 
 void setup() {
   Serial.begin(57600);
   u8g2.begin();
   u8g2.setBusClock(400000);
+  u8g2.setContrast(0);
+  oled_active = false;
 
-  pinMode(1, INPUT); // Sensor water temperature
-  pinMode(2, INPUT); // Sensor ambient temperature
+  pinMode(1, INPUT);  // Sensor water temperature
+  pinMode(2, INPUT);  // Sensor ambient temperature
   pinMode(OC1A_PIN, OUTPUT);
 
   // Default number of readings prior to calculate and send PWM signal and data
   readCount = 631;
+  deltaTOld = 0.0;
 
   // Default sigmoid parameters - see https://www.geogebra.org/calculator with file in project folder for visualization
-  s_min = 18;     // minimum PWM level lift in order to prevent fans from stopping (changes with v)
-  s_max = 100;    // maximum PWM level to be reached
-  s_slope = -0.4; // slope -> how big deltaT has to be to reach a cerain pwm level
-  s_attack = 6.0; // how far the sigmoid function is shifted to the right
+  s_min = 18;      // minimum PWM level lift in order to prevent fans from stopping (changes with v)
+  s_max = 100;     // maximum PWM level to be reached
+  s_slope = -0.4;  // slope -> how big deltaT has to be to reach a cerain pwm level
+  s_attack = 6.0;  // how far the sigmoid function is shifted to the right
 
   T0 = 25 + 273.15;  // Temperature T0 from datasheet, conversion from Celsius to kelvin
 
@@ -57,12 +62,12 @@ void loop() {
     parseSerial();
   }
 
-  // Read temperatures
+  // Read temperatures - both Sensors are off about 0.7°C
   sRad = readNTCTemp(1, 3976) - 0.7;
   sAmb = readNTCTemp(2, 3976) - 0.7;
-  deltaT = (sRad - sAmb);
 
-  // Calculate dT and PWM Value
+  // Calculate dT and PWM value
+  deltaT = (sRad - sAmb);
   if (static_pwm > 0) {
     pwm = static_pwm;
   } else {
@@ -83,21 +88,26 @@ void loop() {
     Serial.println(pwm);
   }
 
-  if (readCount < 10) {
-    if (millis() - lastRefreshTime > 1000) {
+  // Draw data on display
+  if (oled_active && deltaT < 4.0 ) {
+    u8g2.setPowerSave(1);
+    oled_active = false;
+
+  } else if (!oled_active && deltaT >= 4.0) {
+    u8g2.setPowerSave(0);
+    oled_active = true;
+
+  } else if (oled_active) {
+    if (abs(deltaT - deltaTOld) > 0.1) {
       drawOLED();
-      lastRefreshTime = millis();
+      deltaTOld = deltaT;
     }
-  } else {
-    drawOLED();
-    lastRefreshTime = millis();
   }
 }
 
-
 void drawOLED() {
-  u8g2.clearBuffer(); // clear the internal memory
-  u8g2.setFont(u8g2_font_profont22_mn); // choose a suitable font
+  u8g2.clearBuffer();  // clear the internal memory
+  u8g2.setFont(u8g2_font_profont22_mn);  // choose a suitable font
 
   u8g2.setCursor(0, 15);
   u8g2.print(sRad, 1);
@@ -115,25 +125,25 @@ void drawOLED() {
 void parseSerial() {
   byte msg = Serial.read();
   switch (msg) {
-    case 'r': // r for readCount
+    case 'r':  // r for readCount
       readCount = Serial.parseInt();
       break;
-    case 't': // t for top
+    case 't':  // t for top
       s_max = Serial.parseInt();
       break;
-    case 'l': // l for lift
+    case 'l':  // l for lift
       s_min = Serial.parseInt();
       break;
-    case 's': // s for slope
+    case 's':  // s for slope
       s_slope = Serial.parseFloat();
       break;
-    case 'a': // a for attack
+    case 'a':  // a for attack
       s_attack = Serial.parseFloat();
       break;
-    case 'f': // f for forced value
+    case 'f':  // f for forced value
       static_pwm = Serial.parseInt();
       break;
-    case 'x': // x for multiple values from python gui
+    case 'x':  // x for multiple values from python gui
       String str = Serial.readString();
       int n = str.length();
       char buff[n + 1];
@@ -154,15 +164,15 @@ void parseSerial() {
 // https://www.electronics-tutorials.ws/io/thermistors.html
 float readNTCTemp(int analogPin, int BValue) {
   float sum = 0;
-  VCC = 5.0; // Reference Voltage
+  VCC = 5.0;  // Reference Voltage
   for (int i = 0; i < readCount; i++) {
-    VRT = analogRead(analogPin);      //Acquisition analog value of VRT
-    VRT = (VCC / 1023) * VRT;         //Conversion to voltage
+    VRT = analogRead(analogPin);      // Acquisition analog value of VRT
+    VRT = (VCC / 1023) * VRT;         // Conversion to voltage
     VR = VCC - VRT;
-    RT = VRT / (VR / R);              //Resistance of RT
+    RT = VRT / (VR / R);              // Resistance of RT
     ln = log(RT / R);
-    TX = (1 / ((ln / BValue) + (1 / T0))); //Temperature from thermistor
-    TX = TX - 273.15;                 //Conversion to Celsius
+    TX = (1 / ((ln / BValue) + (1 / T0)));  // Temperature from thermistor
+    TX = TX - 273.15;                 // Conversion to Celsius
     sum += TX;
   }
   return sum / readCount;
